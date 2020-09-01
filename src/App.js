dabbf876f7df9dfcddfd36e5ab13268d2364ba34
lexiconPDX <chaungemail@gmail.com>
2020-09-01 00:01:10 -0700

    now it is pink

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
+
 class App extends Component {
   state = {
     ninjas : [
       {name: 'Ryu', age: 30, belt: 'black', id: 1},
-      {name: 'Yoshi', age: 20, belt: 'green', id: 2}
+      {name: 'Yoshi', age: 20, belt: 'green', id: 2},
+      {name: 'Grendel', age: 1110, belt: 'decayed', id: 3}
     ]
+  }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    });
+    this.setState({
+      ninjas: ninjas
+    })
   }
   render() {
     return (
       <div className="App">
         <h1>My First React App</h1>
-        <p>Welcome</p>
-        <Ninjas ninjas={this.state.ninjas} />
+        <p>Welcome, sensei</p>
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas} />
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     )
   }
 }
 
 export default App;
+
+//https://www.youtube.com/watch?v=NFYJZyZIa70&list=PL4cUxeGkcC9ij8CfkAY2RAGb-tmkNwQHG&index=20
+
+//Recap and Virtual DOM
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,31 +1,27 @@
-import React from 'react'
+import React, {Component} from 'react'
+import './Ninjas.css'
 
-// export const Ninjas = ({ninjas}) => {
-//     const ninjaList = ninjas.map(ninja => {
-//         return (
-//             <div className ="ninja" key={ninja.id}>
-//             <div>Name: { ninja.name }
-//             </div>
-//             <div>Age: { ninja.age }</div>
-//             <div>Belt: { ninja.belt } </div>
-//             </div>
-//         )
-//     })
-const Ninjas = ({ninjas}) => {
-    return (
-        <div className="ninja-list">
-            { 
-                ninjas.map(ninja => {
-                    return ninja.age > 20 ? (
-                        <div className="ninja" key={ninja.id}>
-                        <div>Name: { ninja.name } </div>
-                        <div>Age: { ninja.age } </div>
-                        <div>Belt: { ninja.belt } </div>
-                        </div>
-                    ) : null;
-                })
-            }
+const Ninjas = ({ninjas, deleteNinja}) => {
+    const ninjaList = ninjas.map(ninja => {
+        if(ninja.age > 20){
+    return (        
+        <div className="ninja" key={ninja.id}>
+            <div>Name: { ninja.name } </div>
+            <div>Age: { ninja.age } </div>
+            <div>Belt: { ninja.belt } </div>
+            <button onClick={() => {deleteNinja(ninja.id)}}>Delete ninja</button>
             </div>
-    )
+         ) 
+    } else {
+    return null
+    }
+                
+})
+
+return(
+    <div className="ninja-list">
+        { ninjaList } 
+    </div>
+)
 }
 export default Ninjas
